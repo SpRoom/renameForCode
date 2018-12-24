50fe909cabcf54fe85f01ffcb0675d46a445c417
spec <spectatornan@gmail.com>
2018-12-24 18:32:31 +0800

    Initial Commit

---
+++ renameForCode/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  renameForCode
+//
+//  Created by zyt on 2018/12/24.
+//  Copyright © 2018 zsh. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ renameForCode/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  renameForCode
+//
+//  Created by zyt on 2018/12/24.
+//  Copyright © 2018 zsh. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
